9166dbded1dfa0b352398bc490e2d253615b1329
Kobrius11 <kobrius11@gmail.com>
2023-04-20 15:23:16 +0300

    pridejau sekelta

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+class Irasas():
+    def __init__(self):
+        self.suma: float = 0
+        self.komentaras: str = ''
+
+class Islaidos(Irasas):
+    def __init__(self, gavejas):
+        super().__init__()
+        self.gavejas: str = gavejas
+
+class Pajamos(Irasas):
+    def __init__(self, siuntejas):
+        super().__init__()
+        self.siuntejas: str = siuntejas
+
+class Biudzetas():
+    def __init__(self):
+        self.zurnalas: list = []
+
+    def ataskaita(self):
+        pass
+
+    def balansas(self):
+        pass
+
+    def sukurti_pajamu_irasa(self, obj: Pajamos.obj):
+        pass
+
+    def sukurti_islaidu_irasa(self, obj: Islaidos.obj)
+        pass
+
+
 
