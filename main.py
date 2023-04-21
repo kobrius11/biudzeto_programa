feb6f6df8ae591142203159e981c8d6a22995247
AndriusG <agedvilasplay@gmail.com>
2023-04-21 09:22:53 +0300

    atnaujinti_meniu

--- main.py
+++ main.py
@@ -81,9 +81,48 @@ class Biudzetas():
 
 
 
-# Pagrindinis meniu: ataskaita, balansas, pajamu israsas, islaidu israsas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Pagrindinis meniu: ataskaita, balansas, pajamu israsas, islaidu israsas. Biudzetas: islaidu ir pajamu zurnalas.
+import.os
+biudzetas = Biudzetas()
     
-    
+while True:
     print("-Programa Biudzetas-")
     print("------- Meniu -------\n")
     print("1: Ataskaita")
@@ -91,3 +130,30 @@ class Biudzetas():
     print("3: Pajamu israsas")
     print("4: Islaidu israsas")
     print("0: Uzdaryti programa")
+    choice = input("Iveskite savo pasirinkimą (0-4): ")
+    
+    if choice == "1":
+        os.system('cls')
+
+
+    elif choice == "2":
+        os.system('cls')
+
+
+    elif choice == "3":
+        os.system('cls')
+
+
+    elif choice == "4":
+        os.system('cls')
+
+
+    elif choice == "0":
+        os.system('cls')
+        print('------- Gražios dienos! -------')
+        break
+
+       
+
+
+    
